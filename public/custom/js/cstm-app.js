03c96f528ba26e1a67202893b8579ff08d3d4f93
jevedette <zhuldyztazhmaganbet@gmail.com>
2020-04-29 18:29:03 +0600

    доп файлы

---
+++ public/custom/js/cstm-app.js
@@ -0,0 +1,87 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["/custom/js/cstm-app"],{
+
+/***/ "./resources/assets/js/custom.js":
+/*!***************************************!*\
+  !*** ./resources/assets/js/custom.js ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+console.log('Here');
+$('#tabs').closest('.tabs').addClass('is-centered'); // tabs = Vue.component('tabs-section', {
+//     props: ['name'],
+//     template: `<div>
+//                 <div class="tabs is-centered">
+//                     <ul>
+//                         <li v-for="tab in tabs" :class="{ 'active': tab.isActive }" @click="selectTab(tab)">
+//                             <a>{{ tab.name }}</a>
+//                         </li>
+//                     </ul>
+//                 </div>
+//
+//                 <div class="tabs-content">
+//                     <slot></slot>
+//                 </div>
+//               </div>`,
+//
+//     created() {
+//         this.tabs = this.$children;
+//     },
+//     data: function() {
+//         return {
+//             tabs: []
+//         }
+//     },
+//
+//     methods: {
+//         selectTab(selectedTab) {
+//             this.tabs.forEach(tab => {
+//                 tab.isActive = (tab.name == selectedTab.name);
+//             });
+//         }
+//     }
+// });
+//
+// tab = Vue.component('tab-item', {
+//     props: {
+//         name: {
+//             required: true
+//         },
+//
+//         selected: {
+//             default: false
+//         }
+//     },
+//     template: `
+//         <div v-show="isActive">
+//             <slot></slot>
+//         </div>`,
+//     data() {
+//         return {
+//             isActive: false
+//         };
+//     },
+//
+//     mounted() {
+//         this.isActive = this.selected;
+//     }
+// });
+// root = new Vue({
+//     el: "#root",
+// });
+
+/***/ }),
+
+/***/ 1:
+/*!*********************************************!*\
+  !*** multi ./resources/assets/js/custom.js ***!
+  \*********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /mnt/c/Users/mary_/Desktop/-/web/resources/assets/js/custom.js */"./resources/assets/js/custom.js");
+
+
+/***/ })
+
+},[[1,"/custom/js/manifest"]]]);
