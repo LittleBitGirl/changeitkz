6ecda3e83c8d6949190a5ad9fa00415b94a753e6
jevedette <zhuldyztazhmaganbet@gmail.com>
2020-05-10 04:51:45 +0600

    slider with timer

--- packages/Webkul/Shop/src/Resources/views/home/slider.blade.php
+++ packages/Webkul/Shop/src/Resources/views/home/slider.blade.php
@@ -1,3 +1,11 @@
 <section class="slider-block">
     <image-slider :slides='@json($sliderData)' public_path="{{ url()->to('/') }}"></image-slider>
 </section>
+
+<script type="text/javascript">
+    function makeScroll() {
+        $('.slider-right').click();
+    }
+
+    setInterval(makeScroll, 7000);
+</script>
