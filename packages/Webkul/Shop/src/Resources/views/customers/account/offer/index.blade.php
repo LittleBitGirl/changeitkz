ec93e2f1693b060f46b4be03ea3202ff694bf21a
jevedette <zhuldyztazhmaganbet@gmail.com>
2020-05-17 06:21:14 +0600

    debug

--- packages/Webkul/Shop/src/Resources/views/customers/account/offer/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/customers/account/offer/index.blade.php
@@ -2,7 +2,7 @@
 @section('page_title')
     {{ __('shop::app.customer.account.offer.page-title') }}
 @endsection
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 @section('content-wrapper')
     <div class="account-content">
         @include('shop::customers.account.partials.sidemenu')
