6431e87cce28010acd8d21dca0ff4f7aeea4b7b6
jevedette <zhuldyztazhmaganbet@gmail.com>
2020-06-06 19:24:59 +0600

    auth debug

--- packages/Webkul/Shop/src/Resources/views/products/view/recommended-products.blade.php
+++ packages/Webkul/Shop/src/Resources/views/products/view/recommended-products.blade.php
@@ -1,24 +1,25 @@
 <?php
-    $customer = auth()->guard('customer')->user();
-    $recommendedProducts = $customer->recommended_products();
+$customer = auth()->guard('customer')->user();
 ?>
+@if($customer !== null)
+    <?php $recommendedProducts = $customer->recommended_products(); ?>
+    @if ($recommendedProducts->count())
+        <div class="attached-products-wrapper">
+            <div class="title">
+                {{ __('shop::app.products.recommended-product-title') }}
+                <span class="border-bottom"></span>
+            </div>
 
-@if ($recommendedProducts->count())
-    <div class="attached-products-wrapper">
-        <div class="title">
-            {{ __('shop::app.products.recommended-product-title') }}
-            <span class="border-bottom"></span>
+            <div class="product-grid-4">
+
+                @foreach ($recommendedProducts as $recommended_product)
+
+                    @include ('shop::products.list.card', ['product' => $recommended_product])
+
+                @endforeach
+
+            </div>
+
         </div>
-
-        <div class="product-grid-4">
-
-            @foreach ($recommendedProducts as $recommended_product)
-
-                @include ('shop::products.list.card', ['product' => $recommended_product])
-
-            @endforeach
-
-        </div>
-
-    </div>
+    @endif
 @endif
